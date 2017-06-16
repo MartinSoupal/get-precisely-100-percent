e76c7f36ac7d4b81276e46d0b7d88ec386990455
Martin Šoupal <m.soupal.06@gmail.com>
2017-06-16 14:53:19 +0200

    only one argument. v.0.0.1

--- script_cs.js
+++ script_cs.js
@@ -4,6 +4,8 @@ vm.toDecimals = function (number, decimals) {
     return 0;
 }
 
+//values must be array of values
+//decimals >= 2
 vm.percent = function (values) {
     var total = 0;
     var results = [];
@@ -15,10 +17,12 @@ vm.percent = function (values) {
     }
 
     values.forEach(function (value) {
+        //hodnota není číslo, nebo je záporná
         if ((!angular.isNumber(value)) || (value < 0)) {
             console.error(value + " is not number or smaller then 0!");
             return;
         }
+
         total += value;
     }, this);
 
@@ -27,11 +31,14 @@ vm.percent = function (values) {
         return;
     }
 
+    //získání hodnot pro jednotlivé položky
     values.forEach(function (value) {
         results.push(value / total);
     });
 
+    //výsledky poslány na kontrolu
     results = vm.controlPercent(results, decimals);
+
     return results;
 }
 
@@ -39,28 +46,44 @@ vm.controlPercent = function (results, decimals) {
     var total = 0;
     var roundResults = [];
 
+    //pro kontrolované hodnoty
     results.forEach(function (result) {
+        //zaokrouhlená hodnota
         roundResults.push(vm.toDecimals(result, decimals));
     }, this);
 
     total = roundResults.reduce((a, b) => { return a + b; }, 0);
 
+    //total == 100% (1)
     if (total != 1) {
         var diff = [];
 
+        //pro každou hodnotu získání rozdílu mezi počátečním a zaokrouhleným číslem
         results.forEach(function (result, index) {
+            //rozdíl '0.005' je největší a nejideálnější, který může být a dále hledat netřeba
             if (Math.abs(roundResults[index] - result) == 0.005) {
                 (total > 1) ? (roundResults[index] -= 0.01) : (roundResults[index] += 0.01);
                 return roundResults;
             } else {
+                //ukládání rozdílů
                 diff.push(roundResults[index] - result);
             };
         }, this);
 
         (total > 1) ?
+            /** Najdeme ten největší rozdíl, jelikož v nejkrajnějším případě je původní číslo _._051 a zaokrouhlené je _._1 => rozdíl 0.0049.
+             *  Pak tedy odečtením 0.01 je rozdíl od původního čísla maximálně 0.0051.
+             * 
+             *  Kdybychom hledali nejmenší, v nejkrajnějším případě dostaneme _._49 a zaokrouhlené je _._0 => rozdíl -0.0049.
+             *  Pak tedy odečtením 0.01 je rozdíl od původního čísla maximálně -0.0149 => což je daleko větší než 0.0051 výše zmiňované.
+             * 
+             *  Dojde tedy k nejmenšímu zkreslení reálného výsledku!
+             */
             (roundResults[diff.indexOf(Math.max(...diff))] -= 0.01)
             :
             (roundResults[diff.indexOf(Math.min(...diff))] += 0.01);
+
     }
+
     return roundResults;
 }
